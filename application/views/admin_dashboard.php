964c4887a51ea1124992ea4218afc061c1842f18
Patrick Jerome Uy <83640317+patrickjeromeuy@users.noreply.github.com>
2021-10-15 21:07:57 +0800

    added chart for prices, added table of wallet topups

--- application/views/admin_dashboard.php
+++ application/views/admin_dashboard.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.1/chart.js" integrity="sha512-b3xr4frvDIeyC3gqR1/iOi6T+m3pLlQyXNuvn5FiRrrKiMUJK3du2QqZbCywH6JxS5EOfW0DY0M6WwdXFbCBLQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="<?php echo base_url('assets/css/admin_dashboard_styles.css')?>">
     <title>Admin Dashboard</title>
 </head>
@@ -85,9 +86,11 @@
         <div id="app" class="page-container">
             <?php echo $this->pagination->create_links(); ?>
         </div>
+        
         <div class="btn-logout">
 			<a href="<?php echo base_url();?>admin/logout">LOG OUT</a>
 		</div>
     </div>
+
 </body>
 </html>
