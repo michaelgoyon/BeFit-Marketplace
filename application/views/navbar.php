86ffe3e6938288df27e43dfca70ed31b5652f97a
mjgoyon <201811420@feualabang.edu.ph>
2021-10-21 14:58:59 +0800

    UPDATES 10-21-21
    fixed design
    added wallet balance on navbar

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -7,13 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/navbar_styles.css') ?>">
     <script src="https://unpkg.com/paymaya-js-sdk@2.0.0/dist/bundle.js"></script>
 </head>
 <div class="nav">
     <ul class="items">
-        <li><a href="<?php echo base_url('user/marketplace/') ?>"><img class="navlogo" src="<?php echo base_url('assets/images/befitlogo.png') ?>"></a></li>
+        <li><a href="<?php echo base_url('user/marketplace/') ?>"><img class="navlogo"src="<?php echo base_url('assets/images/befitlogo.png') ?>"></a></li>
         <li class="navitems"><a href="<?php echo base_url('user/marketplace/') ?>">Marketplace</a></li>
         <li class="navitems"><a href="<?php echo base_url('user/podcast/') ?>">Podcast</a></li>
         <li class="navitems"><a href="<?php echo base_url('user/nutrition/') ?>">Nutrition</a></li>
@@ -22,16 +23,27 @@
 
     </ul>
     <ul class="items2">
+        <div class="wallet">
+            <?php
+                foreach ($users as $row) {
+            ?>
+            <p>PHP<?php echo $row->users_wallet;?>.00</p>
+            <?php
+                }
+            ?>
+        </div>
         <li class="with-submenu">
             <?php
-            foreach ($users as $row) {
+                foreach ($users as $row) {
             ?>
-                <img src='<?php echo base_url() . 'uploads/' . $row->users_avatar; ?>'>
+            <img src='<?php echo base_url() . 'uploads/' . $row->users_avatar; ?>'>
             <?php
             }
             ?>
             <ul class="submenu">
-                <li><a href="<?php echo base_url('user/profile/' . $this->session->userdata('userusername')) ?>">Profile</a></li>
+                <li><a
+                        href="<?php echo base_url('user/profile/' . $this->session->userdata('userusername')) ?>">Profile</a>
+                </li>
                 <li><a href="<?php echo base_url(); ?>user/topup">Wallet</a></li>
                 <li><a href="#">Bookings</a></li>
                 <li><a href="<?php echo base_url(); ?>user/logout">Log Out</a></li>
